7ab7a82d2f22ad6ec88a647ade28fa02a102c771
bibek@dzangolab <bibek@dzangolab.com>
2021-10-25 22:44:23 +0545

    feat: complete navbar for mobile view

--- src/common/Header/Header.component.tsx
+++ src/common/Header/Header.component.tsx
@@ -4,18 +4,22 @@ import styled from "styled-components";
 export const Header: FC = () => {
   return (
     <HeaderWrapper>
-      <HeaderTitle>win exciting prizes at discount of 50%</HeaderTitle>
+      <HeaderTitle>
+        Hurry up! and Place your orders at discount of <strong>50%</strong>
+      </HeaderTitle>
     </HeaderWrapper>
   );
 };
 
 const HeaderWrapper = styled.header`
   background-color: #5d3fd3;
-  height: 0.2rem;
+  line-height: 1.5rem;
+  height: 1.3rem;
   padding: 1rem;
   text-align: center;
 `;
 const HeaderTitle = styled.p`
   color: #fff;
-  margin-top: -0.75rem;
+  font-size: 0.9rem;
+  margin-top: -0.8rem;
 `;
--- src/common/Navbar/Navbar.component.tsx
+++ src/common/Navbar/Navbar.component.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { BellIcon, SearchIcon, ShoppingIcon } from "../../assets/Icons/Icons";
 
 export const Navbar: FC = () => {
+  const handleClick = () => {};
+
   return (
     <NavbarWrapper>
       <MainContentWrapper>
@@ -10,7 +14,22 @@ export const Navbar: FC = () => {
             <option value="English">EN</option>
             <option value="Nepali">NEP</option>
           </LanguageWrapper>
+          <LogoWrapper>
+            <Link to="/">EF</Link>
+          </LogoWrapper>
         </SearchWrapper>
+        <SearchBoxWrapper>
+          <SearchInput
+            type="text"
+            placeholder="search..."
+            name="search"
+          ></SearchInput>
+          <SearchIcon onClick={handleClick}></SearchIcon>
+        </SearchBoxWrapper>
+        <CartWrapper>
+          <BellIcon></BellIcon>
+          <ShoppingIcon></ShoppingIcon>
+        </CartWrapper>
       </MainContentWrapper>
     </NavbarWrapper>
   );
@@ -18,10 +37,46 @@ export const Navbar: FC = () => {
 
 const NavbarWrapper = styled.nav`
   background-color: whitesmoke;
-  padding: 1rem;
+  padding: 0.75rem;
 `;
-const MainContentWrapper = styled.section``;
 
-const SearchWrapper = styled.nav``;
+const MainContentWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-const LanguageWrapper = styled.select``;
+const SearchWrapper = styled.section`
+  display: flex;
+  flex-directon: row;
+  align-items: center;
+`;
+
+const SearchBoxWrapper = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const LanguageWrapper = styled.select`
+  font-size: 0.7rem;
+  height: 1.5rem;
+  margin: 0rem 1%;
+`;
+
+const LogoWrapper = styled.section`
+  display: none;
+`;
+
+const SearchInput = styled.input`
+  height: 1.15rem;
+  margin-bottom: 0.1rem;
+  margin: 0rem 0.3%;
+`;
+
+const CartWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
