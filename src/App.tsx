1e6b1f4b1ec2664ed2b1dc403eccf410348b6d9a
bibek@dzangolab <bibek@dzangolab.com>
2021-10-25 20:47:27 +0545

    feat: add react router dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Homepage from "./components/HomePage/Homepage.component";
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Homepage}></Route>
+      </Switch>
+    </Router>
   );
-}
-
+};
 export default App;
---
+++ src/components/HomePage/Homepage.component.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Homepage: FC = () => {
+  return (
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  );
+};
+
+export default Homepage;
