63efa5c2c0170197c36243840ad2d7cb6de5553b
bibek@dzangolab <bibek@dzangolab.com>
2021-10-25 21:04:11 +0545

    feat: add navbar component

---
+++ src/common/Navbar/Navbar.component.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+export const Navbar: FC = () => {
+  return (
+    <NavbarWrapper>
+      <MainContentWrapper>
+        <SearchWrapper>
+          <LanguageWrapper>
+            <option value="English">EN</option>
+            <option value="Nepali">NEP</option>
+          </LanguageWrapper>
+        </SearchWrapper>
+      </MainContentWrapper>
+    </NavbarWrapper>
+  );
+};
+
+const NavbarWrapper = styled.nav`
+  background-color: #000;
+`;
+const MainContentWrapper = styled.section``;
+
+const SearchWrapper = styled.nav``;
+
+const LanguageWrapper = styled.select``;
